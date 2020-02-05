c6613f83fceb723eaf0ec8864268674ff460ac5d
Brian <brian.moraa@ucuenca.edu.ec>
2020-02-05 12:08:35 -0500

    no message

--- model/database.php
+++ model/database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function StartUp()
     {
-        $pdo = new PDO('mysql:host=dbserverbi.database.windows.net;dbname=Hospitales', 'dbserverbi.database.windows.net', 'Universidad2020');
+        $pdo = new PDO('mysql:host=dbserverbi.database.windows.net;dbname=Hospitales', 'dbserverbi', 'Universidad2020');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
     }
