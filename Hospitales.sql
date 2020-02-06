76c0249b3d4ba61a6399f9f8e516c8ef484ecd46
Brian <brian.moraa@ucuenca.edu.ec>
2020-02-06 11:18:28 -0500

    test

--- Hospitales.sql
+++ Hospitales.sql
@@ -1,9 +1,9 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-CREATE SCHEMA IF NOT EXISTS Hospitales DEFAULT CHARACTER SET utf8 ;
-USE  Hospitales;
-			CREATE TABLE IF NOT EXISTS Hospitales.Facultad (
+CREATE SCHEMA IF NOT EXISTS Hospital DEFAULT CHARACTER SET utf8 ;
+USE  Hospital;
+			CREATE TABLE IF NOT EXISTS Hospital.Facultad (
 				idFacultad INT NOT NULL,				
 				nombre VARCHAR (40) NOT NULL,				
 						PRIMARY KEY (idFacultad)
@@ -11,7 +11,7 @@ USE  Hospitales;
 )
 			ENGINE = InnoDB;
 
-			CREATE TABLE IF NOT EXISTS Hospitales.Carrera (
+			CREATE TABLE IF NOT EXISTS Hospital.Carrera (
 				idCarrera INT NOT NULL,				
 				nombre VARCHAR (40) NOT NULL,				
 				idFacultad INT NOT NULL,				
@@ -19,13 +19,13 @@ USE  Hospitales;
 
 				CONSTRAINT fk_Carrera_Facultad1
 					FOREIGN KEY (idFacultad) 
-					REFERENCES Hospitales.Facultad (idFacultad)
+					REFERENCES Hospital.Facultad (idFacultad)
 					ON DELETE NO ACTION
     				ON UPDATE NO ACTION
 )
 			ENGINE = InnoDB;
 
-			CREATE TABLE IF NOT EXISTS Hospitales.Estudiante (
+			CREATE TABLE IF NOT EXISTS Hospital.Estudiante (
 				idEstudiante VARCHAR (40) NOT NULL,				
 				nombre VARCHAR (40) NOT NULL,				
 				apellido VARCHAR (40) NOT NULL,				
@@ -38,7 +38,7 @@ USE  Hospitales;
 
 				CONSTRAINT fk_Estudiante_Carrera1
 					FOREIGN KEY (idCarrera) 
-					REFERENCES Hospitales.Carrera (idCarrera)
+					REFERENCES Hospital.Carrera (idCarrera)
 					ON DELETE NO ACTION
     				ON UPDATE NO ACTION
 )
